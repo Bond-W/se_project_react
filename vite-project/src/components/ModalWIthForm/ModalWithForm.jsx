30e138d42bbc6beca02201e9faeeedf08ac41e7f
bond-wyatt <bondwyatt36@gmail.com>
2025-01-27 19:30:04 -0600

    location and tempurature functional

--- vite-project/src/components/ModalWIthForm/ModalWithForm.jsx
+++ vite-project/src/components/ModalWIthForm/ModalWithForm.jsx
@@ -1,11 +1,11 @@
 import "./ModalWIthForm.css"
 
-function ModalWithForm({ children, buttonText, title, activeModal, closeActiveModal }) {
+function ModalWithForm({ children, buttonText, title, activeModal, onClose }) {
   return (
     <div className={`modal ${activeModal === "add-garment" && "modal_opened"}`}>
         <div className="modal__content">
         <h2 className="modal__title">{title}</h2>
-        <button onClick={closeActiveModal} type="button" className="modal__close">
+        <button onClick={onClose} type="button" className="modal__close">
         </button>
       <form className="modal__form">
         {children}
--- vite-project/src/components/WeatherCard/WeatherCard.jsx
+++ vite-project/src/components/WeatherCard/WeatherCard.jsx
@@ -1,13 +1,39 @@
-import "./WeatherCard.css"
-import sunny from "../../assets/sunny.png"
+import "./WeatherCard.css";
+import { weatherOptions, deafultWeatherOptions } from "../../utils/constants";
 
-function WeatherCard() {
+
+function WeatherCard({ weatherData }) {
+  const filteredOptions = weatherOptions.filter((option) => {
     return (
-        <section className="weather-card">
-            <p className="weather-card__temp">75&deg;F</p>
-            <img src={sunny} alt="sunny" className="weather-card__image" />
-        </section>
-    )
+      option.day === weatherData.isDay &&
+      option.condition === weatherData.condition
+    );
+  });
+
+
+  let weatherOption;
+  if (filteredOptions.length === 0) {
+    weatherOption = deafultWeatherOptions[weatherData.isDay ? "day" : "night"];
+  } else {
+      weatherOption = filteredOptions[0];
+  }
+
+
+
+
+
+  return (
+    <section className="weather-card">
+      <p className="weather-card__temp">{weatherData.temp.F} &deg;F</p>
+      <img
+        src={weatherOption?.url}
+        alt={`Card showing ${
+          weatherOption?.day ? "day" : "night"
+        }time ${weatherOption?.condition} weather`}
+        className="weather-card__image"
+      />
+    </section>
+  );
 }
 
 export default WeatherCard;
